90559c8ad0a7bae7209f16c1abdac91a4a18a031
Sauyon Lee <git@sauyon.com>
2013-11-16 13:08:45 -0800

    working chat server

--- moechat.go
+++ moechat.go
@@ -8,36 +8,38 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path == "/" {
-		body, err := ioutil.ReadFile("/srv/chat/index.html")
-		if err != nil {
-			errorHandler(w, r, err)
-			return
-		}
-		fmt.Fprintf(w, string(body))
-	} else {
-		body, err := ioutil.ReadFile("/srv/chat" + r.URL.Path)
-		if err != nil {
-			errorHandler(w, r, err)
-			return
-		}
-		fmt.Fprintf(w, string(body))
+	fmt.Println("Handling connection: " + r.URL.Path)
+	name := "/srv/chat" + r.URL.Path
+	if r.URL.Path != "/" {
+		name = "/srv/chat" + r.URL.Path
 	}
+	file, err := os.Open(name)
+	if err != nil {
+		errorHandler(w, r, err)
+		return
+	}
+	finfo, err := file.Stat()
+	if err != nil {
+		errorHandler(w, r, err)
+		return
+	}
+	http.ServeContent(w, r, name, finfo.ModTime(), file)
 }
 
 func errorHandler(w http.ResponseWriter, r *http.Request, err error) {
+	fmt.Println("Error handling request: " + err.Error())
 	if os.IsNotExist(err) {
 		body, err := ioutil.ReadFile("/srv/chat/404.html")
 		if err == nil {
 			fmt.Fprintf(w, string(body))
-			return
 		}
 	}
-	fmt.Fprintf(w, err.Error())
 }
 
 func main() {
 	fmt.Printf("Starting MoeChat!\n")
+	go h.run()
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/chat", chatHandler)
 	http.ListenAndServe(":8080", nil)
 }
