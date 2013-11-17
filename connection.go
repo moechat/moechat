15948cb55fb48e49a1a49bc3cc8f6921c5c145fa
Sauyon Lee <git@sauyon.com>
2013-11-16 19:22:44 -0800

    fix version checking

--- connection.go
+++ connection.go
@@ -36,7 +36,7 @@ func (c *connection) reader() {
 		code, msg := smsg[0], smsg[1]
 		switch code {
 		default: log.Println("Code is not one of m, e, v and u. Code is: " + code)
-		case "v": if(msg != "0.1") c.ws.WriteMessage(websocket.TextMessage, `{"error":"Client out of date!"}`); break
+		case "v": if(msg != "0.2") { c.ws.WriteMessage(websocket.TextMessage, `{"error":"Client out of date!"}`); break }
 		case "m": h.broadcast <- []byte(msg)
 		case "e": c.email = msg
 		case "u": c.name = msg
