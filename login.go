600f416e1d0ed8d6085f188cf88a3a17a48633b8
William Meng <wlmeng11@gmail.com>
2013-11-16 20:36:36 -0800

    actual login.go

---
+++ login.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"net/http"
+)
+
+var users map[string]User = make(map[string]User)
+
+type User struct {
+	Name string `json:"username"`
+	Email string `json:"email"`
+}
+
+/* Checks login info against database and return a token */
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+
+	//token := generateToken()
+
+	//users[token] = User{username, email}
+}
