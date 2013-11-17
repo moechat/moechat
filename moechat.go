bc4b956db342954d42caa3c6575108aabe3f9be9
Sauyon Lee <git@sauyon.com>
2013-11-16 16:26:10 -0800

    stop printing requests from blocked addresses

--- moechat.go
+++ moechat.go
@@ -11,7 +11,6 @@ import (
 func handler(w http.ResponseWriter, r *http.Request) {
 	ip := strings.Split(r.RemoteAddr,":")[0]
 	if ip == "54.227.38.194" {
-		fmt.Println("Ignoring request from ip: " + ip)
 		return
 	}
 	fmt.Println("Handling connection from ip " + ip + " for: " + r.URL.Path)
